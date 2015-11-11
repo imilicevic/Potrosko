9be07ff33c11a8893e6a13b8d38f637299bf6fa4
Marko Plaftarić <Marko Plaftari>
2015-11-11 23:08:12 +0100

    Kreiran Home Screen Fragment sa slikom i praznim listView-om

--- app/src/main/java/com/foi/air/potrosko/MainActivity.java
+++ app/src/main/java/com/foi/air/potrosko/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
 }
