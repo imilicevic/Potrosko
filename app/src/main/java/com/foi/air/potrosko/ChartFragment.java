d43e186df2ad7f6e1bf49ecca92cb64f6594bbb4
imilicevic <ivamilicevi@foi.hr>
2015-11-11 23:16:43 +0100

    Conflict resolved

---
+++ app/src/main/java/com/foi/air/potrosko/ChartFragment.java
@@ -0,0 +1,29 @@
+package com.foi.air.potrosko;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ChartFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ChartFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ChartFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chart, container, false);
+    }
+
+}
--- app/src/main/java/com/foi/air/potrosko/MainActivity.java
+++ app/src/main/java/com/foi/air/potrosko/MainActivity.java
@@ -1,16 +1,19 @@
 package com.foi.air.potrosko;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
