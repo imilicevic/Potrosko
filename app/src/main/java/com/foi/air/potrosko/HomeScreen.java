a4b3c78710fec494d1406447f926c86ae9031462
Marko Plaftarić <Marko Plaftari>
2015-11-11 23:09:12 +0100

    Drugi dio commit-a

---
+++ app/src/main/java/com/foi/air/potrosko/HomeScreen.java
@@ -0,0 +1,19 @@
+package com.foi.air.potrosko;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeScreen extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_screen, container, false);
+    }
+}
