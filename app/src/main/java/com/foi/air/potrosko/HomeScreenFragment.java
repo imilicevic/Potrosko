6dd0892308f5b02ef5f4a894e5788c1121fa74eb
Marko Plaftarić <Marko Plaftari>
2015-11-15 15:19:38 +0100

    Kreiran drawer koji linka na fragmente HomeScreen i Chart

--- app/src/main/java/com/foi/air/potrosko/HomeScreenFragment.java
+++ app/src/main/java/com/foi/air/potrosko/HomeScreenFragment.java
@@ -11,13 +11,13 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class ChartFragment extends Fragment {
+public class HomeScreenFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chart, container, false);
+        return inflater.inflate(R.layout.fragment_home_screen, container, false);
     }
 
     @Override
@@ -25,23 +25,24 @@ public class ChartFragment extends Fragment {
         // TODO Srediti listView dummy podatke
         super.onActivityCreated(savedInstanceState);
         Log.d("CreatedActivity", "LaunchpadFragment");
-        ListView plaftaList= (ListView) getActivity().findViewById(R.id.listViewChart);
+        ListView plaftaList= (ListView) getActivity().findViewById(R.id.listViewHome);
         // Gets the ListView from the View list of the parent activity
         // Gets a CursorAdapter
-        String[] sky = {"Dal", "Je", "Ljubav", "Ili", "Pivo"};
+        String[] ajs = {"Esi", "Mi", "Dobar", "Essi", "Mii", "Dobaaaar"};
         ArrayAdapter<String> plaftaAdapter;
-        plaftaAdapter= new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, sky);
+        plaftaAdapter= new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, ajs);
         plaftaList.setAdapter(plaftaAdapter);
         // On list item clik show Toast msg with the content of item
         plaftaList.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        String sky = String.valueOf(parent.getItemAtPosition(position));
-                        Toast.makeText(getActivity(), sky, Toast.LENGTH_SHORT).show();
+                        String ajs = String.valueOf(parent.getItemAtPosition(position));
+                        Toast.makeText(getActivity(), ajs, Toast.LENGTH_SHORT).show();
                     }
                 }
         );
 
     }
+
 }
