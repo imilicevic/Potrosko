1c87fc4f144422c86193162810f8e2fad3e2ab94
Marko Plaftarić <Marko Plaftari>
2015-11-15 15:25:09 +0100

    Commit prije resolvanja conflicta

--- app/src/main/java/com/foi/air/potrosko/MainActivity.java
+++ app/src/main/java/com/foi/air/potrosko/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         // Set a Toolbar to replace the ActionBar.
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
