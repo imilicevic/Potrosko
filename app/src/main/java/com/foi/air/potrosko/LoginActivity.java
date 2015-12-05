eecfbaae318fc2ca86495e37ce886fba39e64e50
Andrej <andrej.kasic7@gmail.com>
2015-12-05 12:27:44 +0100

    Improve login logic(without pressing button) and maximize input characters

--- app/src/main/java/com/foi/air/potrosko/LoginActivity.java
+++ app/src/main/java/com/foi/air/potrosko/LoginActivity.java
@@ -1,24 +1,14 @@
 package com.foi.air.potrosko;
 
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
 import android.content.Intent;
-import android.view.View;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileOutputStream;
-
-import butterknife.ButterKnife;
-import butterknife.InjectView;
 
 /**
  * Created by Andrej on 14.11.2015..
@@ -44,8 +34,41 @@ public class LoginActivity extends AppCompatActivity{
         // Pin input text
         txtPin = (EditText) findViewById(R.id.input_pin);
 
+        // Login with pressing soft keyboard key
+        txtPin.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_GO) {
+                    // do your stuff here
+                    // Get pin from EditText
+                    String pin = txtPin.getText().toString();
+
+                    // Check if pin is filled
+                    if(pin.trim().length() > 0){
+                        // For testing pin is checked with sample data
+                        // pin = 1234
+                        if(pin.equals("1234")){
+                            // Starting MainActivity
+                            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(i);
+                            finish();
+
+                        }else{
+                            // pin doesn't match
+                            txtPin.setError("PIN is incorrect");
+                        }
+                    }else{
+                        // user didn't entered pin
+                        // Show alert asking him to enter the details
+                        txtPin.setError("Please enter PIN");
+                    }
+                }
+                return false;
+            }
+        });
+
         // Login button
-        btnLogin = (Button) findViewById(R.id.btn_login);
+      /*  btnLogin = (Button) findViewById(R.id.btn_login);
 
         // Login button click event
         btnLogin.setOnClickListener(new View.OnClickListener() {
@@ -76,6 +99,6 @@ public class LoginActivity extends AppCompatActivity{
                 }
 
             }
-        });
+        }); */
     }
 }
