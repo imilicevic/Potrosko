5cb33b17d6c55871c9076933ed72a3f33d475f3d
Marko Plaftarić <marko.plaftaric@gmail.com>
2015-12-29 13:55:04 +0100

    Implementacija onItemLongClickListener-a i Dialog Interface-a

--- app/src/main/java/com/foi/air/potrosko/fragments/HomeScreenFragment.java
+++ app/src/main/java/com/foi/air/potrosko/fragments/HomeScreenFragment.java
@@ -1,6 +1,8 @@
 package com.foi.air.potrosko.fragments;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,13 +10,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.activeandroid.query.Delete;
 import com.foi.air.potrosko.R;
 import com.foi.air.potrosko.core.ListModel;
 import com.foi.air.potrosko.core.ListViewAdapter;
+import com.foi.air.potrosko.core.OnItemClickListener;
+import com.foi.air.potrosko.db.Category;
 import com.foi.air.potrosko.db.Transaction;
+import com.foi.air.potrosko.db.TransactionType;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -42,8 +49,77 @@ public class HomeScreenFragment extends Fragment {
         customAdapter = new ListViewAdapter(this.getActivity(),CustomListViewValuesArr, getResources());
         list.setAdapter(customAdapter);
 
+        // long click listener implementation
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View v, int position, long id) {
+
+                try{
+
+                    //transaction dialogs
+                    AlertDialog.Builder dialogBuilderMain = new AlertDialog.Builder(getActivity());
+                    dialogBuilderMain.setMessage(getActivity().getResources().getString(R.string.transaction_options))
+                            .setPositiveButton(getActivity().getResources().getString(R.string.delete), new DialogInterface.OnClickListener(){
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    switch (which){
+                                        case DialogInterface.BUTTON_POSITIVE:
+
+                                            //delete transaction
+                                            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
+                                            dialogBuilder.setMessage(getActivity().getResources().getString(R.string.delete_transaction))
+                                                    .setPositiveButton(getActivity().getResources().getString(R.string.yes), new DialogInterface.OnClickListener(){
+                                                        @Override
+                                                        public void onClick(DialogInterface dialog, int which) {
+                                                            switch (which){
+                                                                case DialogInterface.BUTTON_POSITIVE:
+
+                                                                    //TODO Logika za brisanje podataka
+
+                                                                    Toast.makeText(getActivity(), "Brisanje podataka", Toast.LENGTH_SHORT).show();
+
+                                                                    //new Delete().from(Item.class).where("remote_id = ?", 1).execute();
+
+                                                                    // remove child element from the list, and delete it from database
+
+                                                                    //propagate changes
+
+                                                                    break;
+                                                            }
+                                                        }
+                                                    })
+                                                    .setNegativeButton(getActivity().getResources().getString(R.string.no),null)
+                                                    .show();
+                                            break;
+                                    }
+                                }
+                            })
+                            .setNegativeButton(getActivity().getResources().getString(R.string.edit), new DialogInterface.OnClickListener(){
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // edit transactions
+
+                                    // TODO Logika za izmjenu podataka se poziva ovdje
+
+                                    Toast.makeText(getActivity(), "Izmjena podataka", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .show();
+                    // return true to say "event handled"
+                    return true;
+
+                }catch (Exception ex){
+                    Toast.makeText(getActivity().getBaseContext(), ex.toString(), Toast.LENGTH_LONG).show();
+                }finally {
+                    //Toast.makeText(getActivity().getBaseContext(), "Long click je uspješan", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
         return v;
     }
+
 
 
     // Function to set data in ArrayList
